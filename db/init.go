4d736985231c47081e78d216c7eaaa07a226adf7
sbbu <jrrmyr@gmail.com>
2025-01-04 19:57:07 +1030

    log

--- db/init.go
+++ db/init.go
@@ -98,6 +98,8 @@ func LoadTestDB(suffix string) (close func() error, err error) {
 	}
 
 	ConnArgs = os.Getenv("DATABASE_URL")
+	fmt.Printf("DATABASE_URL: %s\n", ConnArgs)
+	
 	if ConnArgs == "" {
 		ConnArgs = fmt.Sprintf(
 			"postgres://meguca:meguca@localhost:5432/%s?sslmode=disable&binary_parameters=yes",
